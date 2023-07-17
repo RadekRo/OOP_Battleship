e6855d0466a85c65a2b62cb447c0d9afd44b37cd
RadekRo <r.roclawski@ankara.pl>
2023-07-17 21:03:12 +0200

    Created basics

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Battleship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
