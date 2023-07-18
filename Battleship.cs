d04c852fa1fb697e5746037ae621c4f7843cf112
RadekRo <r.roclawski@ankara.pl>
2023-07-18 17:23:41 +0200

    Created basic Battleship class and implemented it inside the Main Program

---
+++ Battleship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Battleship
+{
+    internal class Battleship
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello BattleShip!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Battleship!");
+            Battleship battleship = new Battleship();
+            battleship.SayHello();
         }
     }
 }
