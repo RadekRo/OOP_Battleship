10aeed5ac618c7734bcd7f5c0e5b1ced3a234a51
Kaszydo <kaszydo@gmail.com>
2023-07-18 17:48:24 +0200

    [Feature] Add Input class with GetDigitInput and GetStringInput. -GetDigitInput reads one key and if it's digit return it. Else return -1 (options start form 1 and board from 0) -GetStringInput reads line. If inputt is null or empty return invalid input message. Else return user input

---
+++ Input.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Battleship
+{
+    internal class Input
+    {
+
+        public int GetDigitInput()
+        {
+            var UserInput = Console.ReadKey();
+            if (char.IsDigit(UserInput.KeyChar))
+            {
+                return Convert.ToInt32(UserInput);
+            }
+            else
+            {
+                return -1;
+            }
+
+
+        }
+
+        public string GetStringInput()
+        {
+            string UserInput = Console.ReadLine();
+            if (!string.IsNullOrEmpty(UserInput))
+            {
+                return UserInput;
+            }
+            else
+            {
+                return "Invalid Input";
+            }
+        }
+    }
+}
