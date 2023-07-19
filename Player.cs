dbd652d0ad1d4ee41a143cc034af3c33d0099c1d
Kaszydo <kaszydo@gmail.com>
2023-07-19 17:27:48 +0200

    [Feature] Shoot mechanic Needs inplementaion of ship and squer class to check if work. Needs testing

--- Player.cs
+++ Player.cs
@@ -23,8 +23,74 @@ namespace OOP_Battleship
         public List<Ship> Fleet { get; set; }
         public bool IsAlive { get; set; } = true;
 
-
         public int Number { get; set; }
+
+
+        private (int x, int y) GetShootCoordinates()
+        {
+            Input input = new Input();
+            string coordinates;
+            do
+            {
+                Console.WriteLine("Input Coordinates");
+                coordinates = input.GetStringInput();
+            }
+            while (input.ValidateStringInput(coordinates) && input.ValidateCoordinates(coordinates));
+            return input.TranslateCoordinates(coordinates);
+
+        }
+
+        private string CheckIfHit((int x, int y) shootCoordinates, Ship ship)
+        {
+
+            foreach (var squer in ship)
+            {
+                if (shootCoordinates == squer.Position && squer.SquerStatus == "ship")
+                {
+                    squer.SquerStatus = "hit";
+                    return "Hit!";
+
+                }
+            }
+            return "Miss!";
+        }
+
+        private bool CheckIfShipSink(string shootResult, Ship ship)
+        {
+            if (shootResult == "Hit")
+            {
+                return ship.TrueForAll(ship.squer.SquerStatus == "hit");
+            }
+            return false;
+        }
+
+        private void ShipSink(Ship ship)
+        {
+            foreach (var squer in ship)
+            {
+                squer.SquerStatus = "sink";
+            }
+        }
+        public string Shoot(Player oponent)
+        {
+            (int x, int y) coordinates = GetShootCoordinates();
+            foreach (var ship in oponent.Fleet)
+            {
+                string shootResult = CheckIfHit(coordinates, ship);
+                if (shootResult == "hit")
+                {
+                    if (CheckIfShipSink(shootResult, ship))
+                    {
+                        ShipSink(ship);
+                        return "Ship sunk!";
+                    }
+                    return "Ship hit!";
+                }
+            }
+            return "Miss!";
+
+        }
+
 
 
 
