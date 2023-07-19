ac3b66bdefb27a87e00edcfe3004543cf56e9a44
Kaszydo <kaszydo@gmail.com>
2023-07-19 15:54:40 +0200

    [Feature] Player class Player object values: Name, Fleet, IsAlive and number. When constructing Player ISalive always start as true. Number will be used for turn order

---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Battleship
+{
+    internal class Player
+    {
+        private string? _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public List<Ship> Fleet { get; set; }
+        public bool IsAlive { get; set; }
+
+        public int Number { get; set; }
+
+        public Player()
+        {
+            this.IsAlive = true;
+        }
+
+
+    }
+
+
+}
