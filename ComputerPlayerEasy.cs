03a843b0771fc70c29f3c98732f6e78837d4aadb
Kaszydo <kaszydo@gmail.com>
2023-07-23 14:31:45 +0200

    [Feature] Coputer player easy mode Choose random coordinates

---
+++ ComputerPlayerEasy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Battleship
+{
+    internal class ComputerPlayerEasy : Player
+    {
+        public override (int x, int y) GetShootCoordinates()
+        {
+            Random r = new Random();
+            return (r.Next(0, 9), r.Next(0, 9));
+
+        }
+    }
+}
