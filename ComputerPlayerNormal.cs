43d2ad775d890645a1d47ddfcf010ee452a611bc
Kaszydo <kaszydo@gmail.com>
2023-07-24 18:09:58 +0200

    [Refactoring]

--- ComputerPlayerNormal.cs
+++ ComputerPlayerNormal.cs
@@ -37,31 +37,30 @@ namespace OOP_Battleship
                         }
 
 
-                        //                    }
-                        //                    if (x + 1 < 10)
-                        //                    {
-                        //                        positionsToCheck.Add((x + 1, y));
+                    }
+                    if (x + 1 < 10)
+                    {
+                        positionsToCheck.Add((x + 1, y));
+                        if (y - 1 >= 0)
+                        {
+                            positionsToCheck.Add((x + 1, y - 1));
+                            positionsToCheck.Add((x, y - 1));
 
-                        //                        if (y - 1 >= 0)
-                        //                        {
-                        //                            positionsToCheck.Add((x + 1, y - 1));
-                        //                            positionsToCheck.Add((x, y - 1));
+                        }
+                        if (y + 1 < 10)
+                        {
+                            positionsToCheck.Add((x + 1, y + 1));
+                            positionsToCheck.Add((x, y + 1));
 
-                        //                        }
-                        //                        if (y + 1 < 10)
-                        //                        {
-                        //                            positionsToCheck.Add((x + 1, y + 1));
-                        //                            positionsToCheck.Add((x, y + 1));
+                        }
 
-                        //                        }
-
-                        //                    }
-                        //                    foreach (var position in positionsToCheck)
-                        //                    {
-                        //                        if (board.ocean.Find(square => square.Position == position && !SquersToExclude.Contains(square) && square.SquerStatus == "empty"))
-                        //                        {
-                        //                            SquersToExclude.Add(square);
-                        //                        }
+                    }
+                    foreach (var position in positionsToCheck)
+                    {
+                        if (board.ocean.Find(square => square.Position == position && !SquersToExclude.Contains(square) && square.SquerStatus == "empty"))
+                        {
+                            SquersToExclude.Add(square);
+                        }
 
                     }
                     positionsToCheck.Clear();
